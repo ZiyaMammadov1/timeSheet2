1316dce005e923ae6438c36b47cf5d9dee48e761
ZiyaMammadov1 <ziyaam@code.edu.az>
2022-07-02 12:11:05 +0400

    add test service

---
+++ TimeSheet/Controllers/testController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using TimeSheet.DatabaseContext;
+using TimeSheet.Entities;
+
+namespace TimeSheet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowOrigin")]
+    public class testController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public testController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult GetDbEmployeeInfo(string fin)
+        {
+            Employee employee = _context.Employees.FirstOrDefault(x => x.fin == fin);
+            if (employee == null)
+            {
+                return StatusCode(400, "Employee not found");
+            }
+            List<DBEmployee> dbEmployees = _context.dBEmployees.Where(x => x.employeeId == employee.id).ToList();
+            if (dbEmployees.Count <= 0)
+            {
+                return StatusCode(400, "dbEmployee not found for this fin");
+            }
+
+            List<string> result = new List<string>(); 
+            foreach (var item in dbEmployees)
+            {
+                result.Add(($"id : {item.id} " + "employeId : {item.employeeId } " + $"databaseId : {item.databaseId} " + $"companyId : {item.companyId} "+ $"departmentId : {item.departmentId} " + $"projectId : {item.projectId} " + $"isActive : {item.isActive} " + $"positionId : {item.positionId} " + $"isDeleted : {item.isDelete} "));
+            }
+
+            return StatusCode(200, result);
+
+
+        }
+    }
+}
